0feb6076242e4c3c67aee1e720ca4e3f114b27f8
jeonkwangsoo <jeonkwangsoo@naver.com>
2023-10-07 15:54:43 +0900

    Add files via upload

---
+++ wininfo.js
@@ -0,0 +1,17 @@
+function wininfoshow() {
+    return  "window.innerWidth : " + window.innerWidth + "<br>" +
+    "window.innerHeight : " + window.innerHeight + "<br>" +
+    "window.outerWidth : " + window.outerWidth + "<br>" +
+    "window.outerHeight : " + window.outerHeight + "<br>" +
+    "window.screen.width : " + window.screen.width + "<br>" +
+    "window.screen.height : " + window.screen.height + "<br>" +	
+    "window.screen.availWidth : " + window.screen.availWidth + "<br>" +
+    "window.screen.availHeight : " + window.screen.availHeight + "<br>" +
+    "window.screen.colorDepth : " + window.screen.colorDepth + "<br>" +
+    "window.screen.picelDepth : " + window.screen.pixelDepth + "<br>" +
+    "window.location.href : " + window.location.href  + "<br>" +
+    "window.location.hostname : " + window.location.hostname + "<br>" +
+    "window.location.pathname : " + window.location.pathname + "<br>" +
+    "window.location.protocol  : " + window.location.protocol + "<br>" +
+    "window.location.assign() : ";
+}
